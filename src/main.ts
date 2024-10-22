fc8fcea6e4081f020fde01a4c2aa0c2b295da5d4
Igor Karaulov <logistic1902@mail.ru>
2024-10-22 23:57:19 +0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import { TransformResponseInterceptor } from '@common/interceptor/transform-response.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from '@common/filter/http-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -19,10 +17,6 @@ async function bootstrap() {
     const port = config.get<number>('APP_PORT');
     const baseUrl = config.get<string>('APP_DOMAIN');
     const serverUrl = config.get<string>('SERVER_URL');
-
-    app.useGlobalFilters(new HttpExceptionFilter());
-    app.useGlobalFilters(new HttpExceptionFilter());
-    app.useGlobalInterceptors(new TransformResponseInterceptor());
 
     app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
     app.use(
@@ -34,26 +28,26 @@ async function bootstrap() {
         }),
     );
 
-    const configDocument = new DocumentBuilder()
-        .setTitle('SMM Doc')
-        .setDescription('Swagger Super-SMM')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .addServer(baseUrl)
-        .addServer(serverUrl)
-        .build();
-    const document = SwaggerModule.createDocument(app, configDocument);
-    SwaggerModule.setup('swagger', app, document, {
-        swaggerOptions: { persistAuthorization: true },
-    });
+    // const configDocument = new DocumentBuilder()
+    //     .setTitle('SMM Doc')
+    //     .setDescription('Swagger Super-SMM')
+    //     .setVersion('1.0')
+    //     .addBearerAuth()
+    //     .addServer(baseUrl)
+    //     .addServer(serverUrl)
+    //     .build();
+    // const document = SwaggerModule.createDocument(app, configDocument);
+    // SwaggerModule.setup('swagger', app, document, {
+    //     swaggerOptions: { persistAuthorization: true },
+    // });
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            whitelist: true,
-            disableErrorMessages: false,
-        }),
-    );
+    // app.useGlobalPipes(
+    //     new ValidationPipe({
+    //         transform: true,
+    //         whitelist: true,
+    //         disableErrorMessages: false,
+    //     }),
+    // );
     // app.useGlobalGuards(
     //     new JwtAuthGuard(app.get(JwtService), app.get(Reflector)),
     // );
--- src/modules/telegram/domain/use-case/telegram.use-case.ts
+++ src/modules/telegram/domain/use-case/telegram.use-case.ts
@@ -1,4 +1,4 @@
-import { Ctx, Start, Update } from "nestjs-telegraf";
+import { Ctx, Message, On, Start, Update } from "nestjs-telegraf";
 import { Telegraf, Scenes } from "telegraf";
 
 interface Context extends Scenes.SceneContext {};
@@ -9,6 +9,19 @@ export class TelegramUseCase extends Telegraf<Context>
     @Start()
     async onStart(@Ctx() ctx: Context)
     {
-        ctx.replyWithHTML( `<b> Привет, ${ctx.from.username} </b>Hello world! `);
+        try
+        {
+            ctx.telegram.sendMessage(ctx.chat.id, `<b>Hello world!</b> `, {parse_mode:'HTML'});
+        }
+        catch(error)
+        {
+            console.error('Error message: ', error.message);
+        }
+    }
+
+    @On('text')
+    async onMessage(@Message('text') message: string, @Ctx() ctx: Context)
+    {
+        ctx.replyWithHTML(`<i>${message}</i>`);
     }
 }
